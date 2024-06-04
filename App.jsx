7b0341b5a1fc1c803c03168feb03aabafd6e60f9
SAMARTHA CHATTERJEE <121757331+samchatt143@users.noreply.github.com>
2024-06-04 21:02:53 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import Product from "./components/Product.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+//import { CartContext } from "./store/shopping-cart-content.jsx";
+import CartContextProvider from "./store/shopping-cart-content.jsx";
+
+function App() {
+  return (
+    <CartContextProvider>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </CartContextProvider>
+  );
+}
+
+export default App;
